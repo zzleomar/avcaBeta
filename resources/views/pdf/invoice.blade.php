9218ca8f96ae8e1b03f2a6eb56c672829e8447de
Jesus <maxhado1993@gmail.com>
2018-01-04 17:52:44 -0400

    Jesus Machado
    Agg, Boleto.pdf

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -11,53 +11,31 @@
   </head>
   <body>
     <img src="img/boleto.png" width="750" height="300"> 
-    <SPAN style="position: absolute; top: 110 px; left: 30 px;">
-      {{$data['nombreapellido']."SIIIIIIIIII"}}
+    
+    <!-- Lado Derecho -->
+     <SPAN style="position: absolute; top: 100 px; left: 30 px;">
+      {{$data['cedula']}}
     </SPAN>
-    <SPAN style="position: absolute; top: 110 px; left: 560 px;">
-      Machado Jesus
+    <SPAN style="position: absolute; top: 130 px; left: 30 px;">
+      {{$data['nombreapellido']}}
     </SPAN>
-    <SPAN style="position: absolute; top: 150 px; left: 30 px;">
-      {{$data['origen']."NOOOOOOOO"}}
+    <SPAN style="position: absolute; top: 170 px; left: 30 px;">
+      {{$data['origen']}}
     </SPAN>
-     <SPAN style="position: absolute; top: 150 px; left: 250 px;">
+    <SPAN style="position: absolute; top: 170 px; left: 250 px;">
       {{$data['idvuelo']}}
     </SPAN>
-     <SPAN style="position: absolute; top: 150 px; left: 350 px;">
+    <SPAN style="position: absolute; top: 170 px; left: 360 px;">
       {{$data['fecha']}}
     </SPAN>
-    <SPAN style="position: absolute; top: 150 px; left: 430 px;">
+    <SPAN style="position: absolute; top: 170 px; left: 440 px;">
        {{$data['hora']}}
     </SPAN>
-     <SPAN style="position: absolute; top: 150 px; left: 560 px;">
-      {{$data['origen_min']}}
-    </SPAN>
-    <SPAN style="position: absolute; top: 150 px; left: 690 px;">
-      {{$data['destino_min']}}
-    </SPAN>
-    <SPAN style="position: absolute; top: 190 px; left: 30 px;">
+     <SPAN style="position: absolute; top: 210 px; left: 30 px;">
       {{$data['destino']}}
     </SPAN>
-    <SPAN style="position: absolute; top: 190 px; left: 555 px;">
-      {{$data['idvuelo']}}
-    </SPAN>
-    <SPAN style="position: absolute; top: 190 px; left: 615 px;">
-      {{$data['fecha']}}
-    </SPAN>
-    <SPAN style="position: absolute; top: 190 px; left: 665 px;">
-      {{$data['hora']}}
-    </SPAN>
-    <SPAN style="position: absolute; top: 240 px; left: 555 px;">
-      {{$data['boletonro']}}
-    </SPAN>
-    <SPAN style="position: absolute; top: 240 px; left: 625 px;">
-      {{$data['embarquehasta']}}
-    </SPAN>
-    <SPAN style="position: absolute; top: 240 px; left: 695 px;">
-      {{$data['asiento']}}
-    </SPAN>
-    <SPAN style="position: absolute; top: 240 px; left: 25 px;">
-      {{$data['boletonro']}}
+    <SPAN style="position: absolute; top: 240 px; left: 350 px;">
+      {{$data['costo']}}
     </SPAN>
     <SPAN style="position: absolute; top: 240 px; left: 110 px;">
       {{$data['embarquehasta']}}
@@ -65,6 +43,46 @@
     <SPAN style="position: absolute; top: 240 px; left: 250 px;">
       {{$data['asiento']}}
     </SPAN>
+
+    <!-- Lado Izquierdo-->
+    <SPAN style="position: absolute; top: 100 px; left: 560 px;">
+      {{$data['cedula']}}
+    </SPAN>
+     <SPAN style="position: absolute; top: 130 px; left: 560 px;">
+      {{$data['nombrecorto']}}
+    </SPAN>
+     <SPAN style="position: absolute; top: 165 px; left: 570 px;">
+      {{$data['origen_min']}}
+    </SPAN>
+    <SPAN style="position: absolute; top: 165 px; left: 705 px;">
+      {{$data['destino_min']}}
+    </SPAN>
+    <SPAN style="position: absolute; top: 205 px; left: 560 px;">
+      {{$data['idvuelo']}}
+    </SPAN>
+    <SPAN style="position: absolute; top: 205 px; left: 620 px;">
+      {{$data['fecha']}}
+    </SPAN>
+    <SPAN style="position: absolute; top: 205 px; left: 700 px;">
+      {{$data['hora']}}
+    </SPAN>
+
+
+    <SPAN style="position: absolute; top: 240 px; left: 560 px;">
+      {{$data['boletonro']}}
+    </SPAN>
+    <SPAN style="position: absolute; top: 250 px; left: 635 px;">
+      {{$data['embarquehasta']}}
+    </SPAN>
+    
+    <SPAN style="position: absolute; top: 240 px; left: 30 px;">
+      {{$data['boletonro']}}
+    </SPAN>
+     <SPAN style="position: absolute; top: 240 px; left: 700 px;">
+      {{$data['asiento']}}
+    </SPAN>
+    
+    
 
  
   
