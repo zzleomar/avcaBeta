b1b46791296e4efe0d307e2e0a3439cfd002557e
zzleomar <dmy.leomar@gmail.com>
2018-01-04 00:16:26 -0400

    Error de compatibilidad id collapse programacion de vuelo

--- resources/views/asistente-RRHH/index.blade.php
+++ resources/views/asistente-RRHH/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
+<h1>BIENVENIDO</h1>
 
 @endsection
--- resources/views/gerente-sucursales/programar-vuelo.blade.php
+++ resources/views/gerente-sucursales/programar-vuelo.blade.php
@@ -1,235 +1,230 @@
 <div class="subtituloM" style="margin-bottom: 20px;">   
-  <h4>ASIGNACIÓN DE PERSONAL Y AERONAVE</h4></div>
-<hr>      
-<div id="AccordionProg" data-children=".item">
-   
+  <h4>ASIGNACIÓN DE PERSONAL Y AERONAVE</h4>
+</div>
+<hr>  
 
-    <a class="btn btn-primary btn-collAc" data-toggle="collapse" data-parent="#AccordionProg" href="#AccordionProg1" aria-expanded="true" aria-controls="AccordionProg1">
+<div id="AccordionProgVuelo" data-children=".item">
+
+    <a class="btn btn-primary btn-collAc" data-toggle="collapse" data-parent="#AccordionProgVuelo" href="#AccordionProgVuelo1" aria-expanded="true" aria-controls="AccordionProgVuelo1">
       Seleción de Piloto
     </a>
  
 
-    <a class="btn btn-primary btn-collAc" data-toggle="collapse" data-parent="#AccordionProg" href="#AccordionProg2" aria-expanded="false" aria-controls="AccordionProg2">
+    <a class="btn btn-primary btn-collAc" data-toggle="collapse" data-parent="#AccordionProgVuelo" href="#AccordionProgVuelo2" aria-expanded="false" aria-controls="AccordionProgVuelo2">
       Selección de Copiloto
     </a>
 
-    <a class="btn btn-primary btn-collAc" data-toggle="collapse" data-parent="#AccordionProg" href="#AccordionProg3" aria-expanded="false" aria-controls="AccordionProg3">
+    <a class="btn btn-primary btn-collAc" data-toggle="collapse" data-parent="#AccordionProgVuelo" href="#AccordionProgVuelo3" aria-expanded="false" aria-controls="AccordionProgVuelo3">
       Selección de Jefe de Cabina
     </a>
  
-    <a class="btn btn-primary btn-collAc" data-toggle="collapse" data-parent="#AccordionProg" href="#AccordionProg4" aria-expanded="false" aria-controls="AccordionProg4">
+    <a class="btn btn-primary btn-collAc" data-toggle="collapse" data-parent="#AccordionProgVuelo" href="#AccordionProgVuelo4" aria-expanded="false" aria-controls="AccordionProgVuelo4">
       Selección de Sobrecargos
     </a>
 
-    <a class="btn btn-primary btn-collAc" data-toggle="collapse" data-parent="#AccordionProg" href="#AccordionProg5" aria-expanded="true" aria-controls="AccordionProg5">
+    <a class="btn btn-primary btn-collAc" data-toggle="collapse" data-parent="#AccordionProgVuelo" href="#AccordionProgVuelo5" aria-expanded="false" aria-controls="AccordionProgVuelo5">
       Seleción de Aeronave
     </a>
-  <div class="item">
-   
-    <div id="AccordionProg1" class="collapse show" role="tabpanel">
-     <div class="card card-body">
-    
-     <div class="table-responsive divtablaAux"> 
-  <table class="table table-hover text-center tablaAux">
-    <thead class="thead-light">
-      <tr>
-        <th>Pilotos</th>
-        <th>Horas de Experiencia</th>
-        <th>Horas de Quincena</th>
-        <th>Asignar</th>
-      </tr>
-    </thead>
-    <?php 
-        $size=sizeof($pilotos);
-        for ($i=0; $i < $size; $i++) { 
-         
-     ?>
-
-    <tbody>
-      <th scope="row">{{ $pilotos[$i]->nombres." ".$pilotos[$i]->apellidos }}</th>
-        <td id="pihe{{ $pilotos[$i]->id }}"></td>
-        <td id="pihp{{ $pilotos[$i]->id }}"></td>
-       <td>
-          <label class="custom-control custom-radio">
-              <input id="piloto{{ $pilotos[$i]->id }}" type="radio" class="custom-control-input pilotoRadio" name="piloto" value="{{ $pilotos[$i]->id }}">
-              <span class="custom-control-indicator"></span>
-            </label>    
-       </td>
-    </tbody>
-    <?php } ?>
-
-  </table>
-  </div>
-    </div>
-  </div>
-  </div>
 
 
-  <div class="item">
-    <div id="AccordionProg2" class="collapse" role="tabpanel">
-      <div class="card card-body">
-    
-        <div class="table-responsive divtablaAux"> 
-           <table class="table table-hover text-center tablaAux">
-               <thead class="thead-light">
-                    <tr>
-                     <th>Copilotos</th>
-                     <th>Horas de Experiencia</th>
-                     <th>Horas de Quincena</th>
-                     <th>Asignar</th>
-                   </tr>
-               </thead>
-    <?php 
-        $size=sizeof($copilotos);
-        for ($i=0; $i < $size; $i++) { 
-         
-     ?>
+      <div class="item">
+       
+        <div id="AccordionProgVuelo1" class="collapse show" role="tabpanel">
+         <div class="card card-body">
+        
+         <div class="table-responsive divtablaAux"> 
+      <table class="table table-hover text-center tablaAux">
+        <thead class="thead-light">
+          <tr>
+            <th>Pilotos</th>
+            <th>Horas de Experiencia</th>
+            <th>Horas de Quincena</th>
+            <th>Asignar</th>
+          </tr>
+        </thead>
+        <?php 
+            $size=sizeof($pilotos);
+            for ($i=0; $i < $size; $i++) { 
+             
+         ?>
 
-      <tbody>
-      <th scope="row">{{ $copilotos[$i]->nombres." ".$copilotos[$i]->apellidos }}</th>
-        <td id="copihe{{ $copilotos[$i]->id }}"></td>
-        <td id="copihp{{ $copilotos[$i]->id }}"></td>
-       <td>
-            <label class="custom-control custom-radio">
-                <input id="copiloto{{ $copilotos[$i]->id }}"  type="radio" class="custom-control-input" name="copiloto" value="{{ $copilotos[$i]->id }}">
-                <span class="custom-control-indicator"></span>
-      
-              </label>      
-       </td>
-       </tbody> 
-    <?php } ?>
+        <tbody>
+          <td>{{ $pilotos[$i]->nombres." ".$pilotos[$i]->apellidos }}</td>
+            <td id="pihe{{ $pilotos[$i]->id }}"></td>
+            <td id="pihp{{ $pilotos[$i]->id }}"></td>
+           <td>
+              <label class="custom-control custom-radio">
+                  <input id="piloto{{ $pilotos[$i]->id }}" type="radio" class="custom-control-input pilotoRadio" name="piloto" value="{{ $pilotos[$i]->id }}">
+                  <span class="custom-control-indicator"></span>
+                </label>    
+           </td>
+        </tbody>
+        <?php } ?>
 
-           </table>
+      </table>
+      </div>
         </div>
       </div>
-     </div>
-  </div>
+      </div>
+
+
+      <div class="item">
+        <div id="AccordionProgVuelo2" class="collapse" role="tabpanel">
+          <div class="card card-body">
+        
+            <div class="table-responsive divtablaAux"> 
+               <table class="table table-hover text-center tablaAux">
+                   <thead class="thead-light">
+                        <tr>
+                         <th>Copilotos</th>
+                         <th>Horas de Experiencia</th>
+                         <th>Horas de Quincena</th>
+                         <th>Asignar</th>
+                       </tr>
+                   </thead>
+        <?php 
+            $size=sizeof($copilotos);
+            for ($i=0; $i < $size; $i++) { 
+             
+         ?>
+
+          <tbody>
+          <td>{{ $copilotos[$i]->nombres." ".$copilotos[$i]->apellidos }}</td>
+            <td id="copihe{{ $copilotos[$i]->id }}"></td>
+            <td id="copihp{{ $copilotos[$i]->id }}"></td>
+           <td>
+                <label class="custom-control custom-radio">
+                    <input id="copiloto{{ $copilotos[$i]->id }}"  type="radio" class="custom-control-input" name="copiloto" value="{{ $copilotos[$i]->id }}">
+                    <span class="custom-control-indicator"></span>
+          
+                  </label>      
+           </td>
+           </tbody> 
+        <?php } ?>
+
+               </table>
+            </div>
+          </div>
+         </div>
+      </div>
   
- <div class="item">
-   <div id="AccordionProg3" class="collapse" role="tabpanel">
-  <div class="card card-body">
-    
-    <div class="table-responsive divtablaAux"> 
-  <table class="table table-hover text-center tablaAux">
-    <thead class="thead-light">
-      <tr>
-        <th>Jefa de Cabina</th>
-        <th>Horas de Experiencia</th>
-        <th>Horas de Quincena</th>
-        <th>Asignar</th>
-      </tr>
-    </thead>
-    <?php 
-        $size=sizeof($jefacs);
-        for ($i=0; $i < $size; $i++) { 
-         
-     ?>
-      <tbody>
-      <th scope="row">{{ $jefacs[$i]->nombres." ".$jefacs[$i]->apellidos }}</th>
-        <td id="jche{{ $jefacs[$i]->id }}"></td>
-        <td id="jchp{{ $jefacs[$i]->id }}"></td>
-       <td>
-      <label class="custom-control custom-radio">
-          <input id="jefac{{ $jefacs[$i]->id }}"  type="radio" class="custom-control-input" name="jefac" value="{{ $jefacs[$i]->id }}">
-          <span class="custom-control-indicator"></span>
-        </label>
-       </td>
-       </tbody> <?php } ?>
-    
-  </table>
-  </div>
-  </div>
-</div>
-  </div>
-
-
- <div class="item">
-   
-    <div id="AccordionProg4" class="collapse" role="tabpanel">
-     
-     <div class="card card-body">
-    
-      <div class="table-responsive divtablaAux"> 
-  <table class="table table-hover text-center tablaAux">
-    <thead class="thead-light">
-      <tr>
-        <th>Sobrecargos</th>
-        <th>Horas de Experiencia</th>
-        <th>Horas de Quincena</th>
-        <th>Asignar</th>
-      </tr>
-    </thead>
-
-    <?php 
-        $size=sizeof($sobrecargos);
-        for ($i=0; $i < $size; $i++) { 
-         
-     ?>
-    <tbody>
-     
-      <th scope="row">{{ $sobrecargos[$i]->nombres." ".$sobrecargos[$i]->apellidos }}</th>
-        <td id="sohe{{ $sobrecargos[$i]->id }}"></td>
-        <td id="sohp{{ $sobrecargos[$i]->id }}"></td>
-        <td>
-          <label class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="t{{ $sobrecargos[$i]->id }}" name="sobrecargos[]" onclick="doIt(this)" value="{{ $sobrecargos[$i]->id }}">
-            <span class="custom-control-indicator"></span>
-          </label>
-       </td>
-    </tbody><?php } ?>
-    
-    </table>
-
-
-
-  </div>
-  </div>
-
-
-
-</div>
-
-
-</div>
-<div class="item">
-   
-    <div id="AccordionProg5" class="collapse" role="tabpanel">
-     <div class="card card-body">
-    
-     <div class="table-responsive divtablaAux"> 
-  <table class="table table-hover text-center tablaAux">
-    <thead class="thead-light">
-      <tr>
-        <th>Aeronave</th>
-        <th>Modelo</th>
-        <th>Horas de Vuelo Post-Mantenimiento</th>
-        <th>Asignar</th>
-      </tr>
-    </thead>
-
-    <?php 
-        $size=sizeof($aeronaves);
-        for ($i=0; $i < $size; $i++) { 
-         
-     ?>
-
-    <tbody>
-      <th>{{ $aeronaves[$i]->matricula }}</th>
-      <th>{{ $aeronaves[$i]->modelo }}</th>
-      <td id="aehm{{ $aeronaves[$i]->id }}"></td>
-       <td>
+     <div class="item">
+       <div id="AccordionProgVuelo3" class="collapse" role="tabpanel">
+      <div class="card card-body">
+        
+        <div class="table-responsive divtablaAux"> 
+      <table class="table table-hover text-center tablaAux">
+        <thead class="thead-light">
+          <tr>
+            <th>Jefa de Cabina</th>
+            <th>Horas de Experiencia</th>
+            <th>Horas de Quincena</th>
+            <th>Asignar</th>
+          </tr>
+        </thead>
+        <?php 
+            $size=sizeof($jefacs);
+            for ($i=0; $i < $size; $i++) { 
+             
+         ?>
+          <tbody>
+          <th scope="row">{{ $jefacs[$i]->nombres." ".$jefacs[$i]->apellidos }}</th>
+            <td id="jche{{ $jefacs[$i]->id }}"></td>
+            <td id="jchp{{ $jefacs[$i]->id }}"></td>
+           <td>
           <label class="custom-control custom-radio">
-              <input id="aeronave{{ $aeronaves[$i]->id }}" type="radio" class="custom-control-input" name="aeronave" value="{{ $aeronaves[$i]->id }}">
+              <input id="jefac{{ $jefacs[$i]->id }}"  type="radio" class="custom-control-input" name="jefac" value="{{ $jefacs[$i]->id }}">
               <span class="custom-control-indicator"></span>
-            </label>    
-       </td>
-    </tbody>
-    <?php } ?>
-  </table>
-  </div>
+            </label>
+           </td>
+           </tbody> <?php } ?>
+        
+      </table>
+      </div>
+      </div>
     </div>
-  </div>
-  </div>
+      </div>
+
+
+     <div class="item">
+       
+        <div id="AccordionProgVuelo4" class="collapse" role="tabpanel">
+         
+         <div class="card card-body">
+        
+          <div class="table-responsive divtablaAux"> 
+      <table class="table table-hover text-center tablaAux">
+        <thead class="thead-light">
+          <tr>
+            <th>Sobrecargos</th>
+            <th>Horas de Experiencia</th>
+            <th>Horas de Quincena</th>
+            <th>Asignar</th>
+          </tr>
+        </thead>
+
+        <?php 
+            $size=sizeof($sobrecargos);
+            for ($i=0; $i < $size; $i++) { 
+             
+         ?>
+        <tbody>
+         
+          <th scope="row">{{ $sobrecargos[$i]->nombres." ".$sobrecargos[$i]->apellidos }}</th>
+            <td id="sohe{{ $sobrecargos[$i]->id }}"></td>
+            <td id="sohp{{ $sobrecargos[$i]->id }}"></td>
+            <td>
+              <label class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="t{{ $sobrecargos[$i]->id }}" name="sobrecargos[]" onclick="doIt(this)" value="{{ $sobrecargos[$i]->id }}">
+                <span class="custom-control-indicator"></span>
+              </label>
+           </td>
+        </tbody><?php } ?>
+        </table>
+      </div>
+      </div>
+    </div>
+    </div>
+
+  <div class="item">
+     
+      <div id="AccordionProgVuelo5" class="collapse" role="tabpanel">
+       <div class="card card-body">
+      
+       <div class="table-responsive divtablaAux"> 
+    <table class="table table-hover text-center tablaAux">
+      <thead class="thead-light">
+        <tr>
+          <th>Aeronave</th>
+          <th>Modelo</th>
+          <th>Horas de Vuelo Post-Mantenimiento</th>
+          <th>Asignar</th>
+        </tr>
+      </thead>
+
+      <?php 
+          $size=sizeof($aeronaves);
+          for ($i=0; $i < $size; $i++) { 
+           
+       ?>
+
+      <tbody>
+        <td>{{ $aeronaves[$i]->matricula }}</td>
+        <td>{{ $aeronaves[$i]->modelo }}</td>
+        <td id="aehm{{ $aeronaves[$i]->id }}"></td>
+         <td>
+            <label class="custom-control custom-radio">
+                <input id="aeronave{{ $aeronaves[$i]->id }}" type="radio" class="custom-control-input" name="aeronave" value="{{ $aeronaves[$i]->id }}">
+                <span class="custom-control-indicator"></span>
+              </label>    
+         </td>
+      </tbody>
+      <?php } ?>
+    </table>
+    </div>
+      </div>
+    </div>
+    </div>
 </div>
 
 <script type="text/javascript">
